a0ca053d6c1563bcef644ae2edcaba5da5a092b0
GabriFili850 <gabri.filigheddu@postcodelottery.co.uk>
2019-10-09 16:07:14 +0100

    added modal, order summary and backdrop to the UI

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+const orderSummary = (props) => {
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicius burger with the following ingredients:</p>
+        </Aux>
+    )
+}
