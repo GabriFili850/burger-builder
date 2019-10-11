191d5eb619c4234616b8d53ad72d6cb44346b2d2
GabriFili850 <gabri.filigheddu@postcodelottery.co.uk>
2019-10-11 09:54:28 +0100

    burger showing the order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,10 +2,24 @@ import React from 'react';
 import Aux from '../../../hoc/Aux';
 
 const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li>
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>
+            )
+        })
     return (
         <Aux>
             <h3>Your Order</h3>
             <p>A delicius burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+            <p>Continue Checkout?</p>
         </Aux>
     )
 }
+
+export default orderSummary;
